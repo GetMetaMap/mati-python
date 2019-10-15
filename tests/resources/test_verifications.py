60914fd362b257cd852acc11ad8c6885af3adb36
Matin Tamizi <matin@cuenca.com>
2019-10-15 14:18:29 -0500

    vcr'ed

--- tests/resources/test_verifications.py
+++ tests/resources/test_verifications.py
@@ -1,13 +1,9 @@
-import os
-
 import pytest
 
 from mati import Client
 
 
-@pytest.mark.skipif(
-    os.getenv('VCR_UNSAFE') != 'true', reason="can't record this yet"
-)
-def test_verifications(client: Client):
+@pytest.mark.vcr
+def test_retrieve_verification(client: Client):
     verification = client.verifications.retrieve('5d9fb1f5bfbfac001a349bfb')
     assert verification
